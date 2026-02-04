f90e6b724430b45ce5e44b364cd36985346ab3ee
Giovanni <gi0vanni-bot@users.noreply.github.com>
2026-02-04 18:01:30 +0000

    Debug: log pnlByRegime SQL

--- app/controllers/Poly.controller.js
+++ app/controllers/Poly.controller.js
@@ -100,7 +100,7 @@ exports.pnlByRegime = async (req, res) => {
        ${modeWhere}
        GROUP BY btc_regime, pt.mode
        ORDER BY pnl_usd DESC;`,
-      { replacements }
+      { replacements, logging: console.log }
     );
 
     return res.status(200).json({ rows });
